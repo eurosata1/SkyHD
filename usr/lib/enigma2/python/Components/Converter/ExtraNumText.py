d9e28673fd77ee505511ac6d1aaf79bcfac8e5a7
eurosat1 <eurosata.1@gmail.com>
2015-04-23 18:17:01 +0200

    test.

--- usr/lib/enigma2/python/Components/Converter/ExtraNumText.py
+++ usr/lib/enigma2/python/Components/Converter/ExtraNumText.py
@@ -19,7 +19,7 @@ from Components.Converter.Converter import Converter
 from Components.Element import cached
 from time import localtime, strftime
 
-class SkyHDExtraNumText(Converter, object):
+class ExtraNumText(Converter, object):
 	SNRNUM = 0
 	AGCNUM = 1
 	BERNUM = 2
